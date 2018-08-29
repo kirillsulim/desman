34d56c444dbdb1c9aabc2d81d7f7c7a516d92d2a
Kirill Sulim <kirillsulim@gmail.com>
2018-08-29 22:54:12 +0500

    Add status and header info flags

--- src/batman/batman.py
+++ src/batman/batman.py
@@ -4,7 +4,9 @@ import yaml
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-s', action='store_true')
+parser.add_argument('-s', '--status', action='store_true', help='show response status')
+parser.add_argument('-b', '--body', action='store_true', help='show response body')
+parser.add_argument('-H', '--header', action='store_true', help='show response headers')
 parser.add_argument('file', metavar='FILE', type=argparse.FileType('r'))
 
 
@@ -14,19 +16,24 @@ class App:
         with(self.args.file) as f:
             try:
                 reqdata = yaml.load(f)
-                #print(reqdata)
                 self.run_req(reqdata)
             except Exception as e:
                 print(e)
                 raise
-        #print(args)
 
     def run_req(self, reqdata):
         response = requests.request(str(reqdata['method']).lower(), reqdata['url'], params=reqdata['params'], headers=reqdata['headers'], data=reqdata['body'])
-        if self.args.s:
-            print(response.status_code)
-        #print(response.text)
-
+        if self.args.status:
+            print("> Status: {} {}".format(response.status_code, response.reason))
+        if self.args.header:
+            print("> Headers:")
+            for header in response.headers:
+                print(">   {}: {}".format(header, response.headers[header]))
+        if self.args.body:
+            print()
+            print(response.text)
+        if not (self.args.body or self.args.status or self.args.header):
+            print(response.text)
 
 
 def main():
