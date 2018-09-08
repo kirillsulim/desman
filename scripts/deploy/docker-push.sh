caef86ddb2796d8388336a31e167720b6a77e59e
Kirill Sulim <kirillsulim@gmail.com>
2018-09-08 22:11:26 +0500

    Add version

--- scripts/deploy/docker-push.sh
+++ scripts/deploy/docker-push.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Script to push desman to docker repository
 
+if [ -n $1 ]; then
+  TAG=$1
+else
+  exit 1
+fi
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push kirillsulim/desman
+docker push kirillsulim/desman:$TAG
