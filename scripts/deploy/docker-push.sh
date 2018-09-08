75b41c1823f0adbd2e8d82e60b5d29f639c8ee01
Kirill Sulim <kirillsulim@gmail.com>
2018-09-08 22:21:29 +0500

    Test docker push for travis

--- scripts/deploy/docker-push.sh
+++ scripts/deploy/docker-push.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 # Script to push desman to docker repository
 
-if [ -n $1 ]; then
-  TAG=$1
-else
-  exit 1
-fi
-
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push kirillsulim/desman:$TAG
+docker push kirillsulim/desman
